f30e0b7423f05692e32eb252f9540f3a849e92eb
mikelaslam <kipkorir385@gmail.com>
2023-01-04 22:21:36 +0300

    Updated README

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Fish-finder-application
 
-"This application will display different types of fish species  and show it's specific info including English name and and  scientific names by consuming data from a public api." The user will be able to see the fish details and have the ability to search any specific fish
+## Project Description
 
-##
+"This application will display different types of fish species  and show it's specific info including English name and and  scientific names by consuming data from a public api." The user will be able to see the fish details and have the ability to search any specific fish displaying it's image, binomial classification
+
+## Technologies Used
+
+
+HTML
+CSS
+JavaScript
+
+## Project set up
+
+After identifying the appropriate api for our project, we start by creating a private repository in Github. In this case I used an api () from _rapidapi.com_ Clone the repository to the local folder and using _touch_ , create a README.md, index.js, index.html and style.css file. Another option is creating the files from the VS Code. Once we are done creating tyhe files, we run _code ._ in the terminal to open in VS Code. Our main concentrating is on the index.js file as it is the one that bears alot of work here.
+
+## Project Handling
+
 
--- index.js
+++ index.js
@@ -23,23 +23,52 @@ fetch('https://fish-species.p.rapidapi.com/fish_api/group?meta_property=scientif
 //Initializing the rendering process
 //Getting the fish data and rendering the fish to the DOM
 
-function displayFish(fish){
-    fish.forEach(fish => {
-        const fishContainer = document.getElementById("fish-container")
-        const fishDiv = document.createElement("div")
-        fishDiv.className = "fish"
-        fishDiv.innerHTML = `
-      
-       <img src="${fish.img_src_set["2x"]}" alt="fish">
-       <h3> Name: ${fish.name}</h3>
-       <h4> Binomial Name: ${fish.meta.binomial_name}
-       <h4> Class:${fish.meta.scientific_classification.class}
-       <h4> Family: ${fish.meta.scientific_classification.family}
-       <h4> Phylum: ${fish.meta.scientific_classification.phylum}
+function displayFish(fish) {
+  fish.forEach((fish) => {
+    const fishContainer = document.getElementById("fish-container")
+    const fishDiv = document.createElement("div")
+    fishDiv.className = "fish"
 
-        `
-        fishContainer.appendChild(fishDiv)
+    // Creating a like button for each fish
+    const likeButton = document.createElement("button")
+    likeButton.innerHTML = "Like"
+
+    let likeCount = 0
+    likeButton.addEventListener("click", () => {
+      likeCount++
+      console.log(`Like count: ${likeCount}`)
+      likeButton.innerHTML = `Like (${likeCount})`
     })
+
+    // Creating a comment button for each fish
+    const commentButton = document.createElement("button")
+    commentButton.innerHTML = "Comment"
+    const commentsList = document.createElement("ul")
+    commentButton.addEventListener("click", () => {
+      const comment = prompt("Enter your comment:")
+      if (comment) {
+        const commentItem = document.createElement("li")
+        commentItem.innerHTML = comment
+        commentsList.appendChild(commentItem)
+      }
+    })
+
+    fishDiv.innerHTML = `
+      <img src="${fish.img_src_set["2x"]}" alt="fish">
+      <h3> Name: ${fish.name}</h3>
+      <h4> Binomial Name: ${fish.meta.binomial_name}
+      <h4> Class:${fish.meta.scientific_classification.class}
+      <h4> Family: ${fish.meta.scientific_classification.family}
+      <h4> Phylum: ${fish.meta.scientific_classification.phylum}
+      `
+    // Appending the like button, comment button, and comments list to the fishDiv
+    
+    fishDiv.appendChild(likeButton)
+    fishDiv.appendChild(commentButton)
+    fishDiv.appendChild(commentsList)
+
+    fishContainer.appendChild(fishDiv)
+  })
 
 //Working on the search button
 const btn = document.getElementById("search-button")
